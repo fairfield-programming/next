d0d6f265ee11ba0f3b544a7d962734c4bd43ddbf
William McGonagle <wpmcgonagle@gmail.com>
2022-04-03 19:52:37 -0400

    Server Side Rendering!!!

--- src/pages/user.jsx
+++ src/pages/user.jsx
@@ -1,6 +1,31 @@
 import React, { useEffect, useState } from "react";
 
-export default function UserPage() {
+export async function getServerData() {
+
+    try {
+        const res = await fetch(`https://fairfield-programming.herokuapp.com/user/6`)
+        
+        if (!res.ok) {
+            throw new Error(`Response failed`)
+        }
+
+        return {
+          props: await res.json(),
+        }
+
+      } catch (error) {
+
+        return {
+          status: 500,
+          headers: {},
+          props: {}
+        }
+
+      }
+
+}
+
+export default function UserPage({ serverData }) {
 
     const [ user, setUser ] = useState({});
     useEffect(() => {
@@ -21,6 +46,6 @@ export default function UserPage() {
 
     }, [])
 
-    return (<h1>Hello There, { user.username }</h1>);
+    return (<h1>Hello There, { serverData.username }</h1>);
 
 }
