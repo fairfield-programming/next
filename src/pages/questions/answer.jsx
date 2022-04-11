fef44ffaa662efba5689c912a18cfd6c74650540
William McGonagle <wpmcgonagle@gmail.com>
2022-04-10 20:10:29 -0400

    Fixed My Idiotness

--- src/pages/questions/answer.jsx
+++ src/pages/questions/answer.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+import { Helmet } from "react-helmet"
 
 /** @jsx jsx */
 import { jsx, Spinner, Flex } from 'theme-ui';
--- src/pages/questions/index.jsx
+++ src/pages/questions/index.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react"
+import { Helmet } from "react-helmet"
+
 import Header from "../../components/header";
 import Footer from "../../components/footer";
 
